230e79cfa292eb2692b89577469e0c25be518b0b
Favor <favoriabs@gmail.com>
2017-05-07 14:46:04 +0100

    added fillable fields in User model

--- app/User.php
+++ app/User.php
@@ -15,8 +15,10 @@ class User extends \Cartalyst\Sentinel\Users\EloquentUser
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'phone_no', 'email', 'password', 'status', 'address', 'profile_picture_path',
-        'title', 'sex', 'postal_address',
+        'first_name', 'last_name', 'phone_no', 'email', 'password', 'status',
+        'address', 'profile_picture_path', 'title', 'sex', 'postal_address',
+        'address_city', 'address_state', 'address_country',
+        'postal_address_city', 'postal_address_state', 'postal_address_country',
     ];
 
     /**
